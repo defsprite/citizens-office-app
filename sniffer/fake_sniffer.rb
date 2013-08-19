4f7056e79736ee971f8b448085e9f4c52b3ebe0f
Christoph Sassenberg <christoph.sassenberg@googlemail.com>
2013-08-19 15:49:28 +0100

    fake it real good.

--- sniffer/fake_sniffer.rb
+++ sniffer/fake_sniffer.rb
@@ -2,11 +2,7 @@
 require 'rubygems'
 require 'eventmachine'
 require 'websocket-eventmachine-client'
-
-@macs = (0..8192).map { (1..6).map { '%02X' % rand(256) }.join(":") }
-
-@sites = ['www.google.co.uk', 'barbican.co.uk', 'bbc.co.uk', 'reddit.com', 'pornhub.com']
-@names = ['Peters-iphone', 'Sabrinas-MacBook', 'Unknown', 'Shawns-iphone-2', 'Julias-iphone']
+require_relative 'fake_data'
 
 EventMachine.run do
 
@@ -27,10 +23,11 @@ EventMachine.run do
 
   EventMachine.tick_loop do
 
-    ip_src   = @sites.sample(1).first
-    wlan_sa  = @macs.sample(1).first
-    dns_name = @names.sample(1).first
-    http_uri = @sites.sample(1).first
+    ip_src   = SITES.sample(1).first
+    wlan_sa  = MACS.sample(1).first
+    dns_name = NAMES.sample(1).first+EXTS.sample(1).first
+
+    http_uri = SITES.sample(1).first
     http_ua  = "Mozilla 5.0 (Fake)"
 
 
